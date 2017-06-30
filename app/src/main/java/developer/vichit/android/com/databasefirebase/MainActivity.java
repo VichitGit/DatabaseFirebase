1724e9e03de9004999f1dc0af484acb027ec7c5c
VichitGit <v.vichitpov@gmail.com>
2017-06-30 18:42:45 +0700

    Add data from firebase to listview

--- app/src/main/java/developer/vichit/android/com/databasefirebase/MainActivity.java
+++ app/src/main/java/developer/vichit/android/com/databasefirebase/MainActivity.java
@@ -3,6 +3,7 @@ package developer.vichit.android.com.databasefirebase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -11,23 +12,30 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
+import static developer.vichit.android.com.databasefirebase.R.id.lvArticle;
+
 public class MainActivity extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
     ListView lvArticles;
+    ArrayList<String> listArticles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lvArticles = (ListView) findViewById(R.id.lvArticle);
+        lvArticles = (ListView) findViewById(lvArticle);
 
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("articles");
         //auto generate key
         String id = databaseReference.push().getKey();
-        databaseReference.child(id).setValue(new Articles(id, "davy"));
+//        databaseReference.child(id).setValue(new Articles(id, "davy"));
+
+        listArticles = new ArrayList<>();
 
 
         /*
@@ -40,7 +48,16 @@ public class MainActivity extends AppCompatActivity {
                     Log.e("ppppp", snapshot.toString()); // Get all data
                     Articles articles = snapshot.getValue(Articles.class); //get data that want
                     Log.e("ppppp", articles.getTitle());
+
+                    listArticles.add(articles.getTitle());
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                            android.R.layout.simple_list_item_1, listArticles);
+                    lvArticles.setAdapter(adapter);
+                    lvArticles.deferNotifyDataSetChanged();
+
+
                 }
+
 
             }
 
